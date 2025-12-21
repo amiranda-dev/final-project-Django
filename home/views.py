3c17fd9918e0d9a4688445e94aa1153c774e0e74
Alisson Miranda <alissonmichelmiranda@gmail.com>
2025-12-21 00:43:40 -0300

    Categoria - Final

--- home/views.py
+++ home/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import *
 from .forms import *
 
@@ -14,10 +14,17 @@ def categoria(request):
     return render(request, 'categoria/lista.html', contexto)
 
 
-def form_categoria(resquest):
-    form = CategoriaForm()
+def form_categoria(request):
+    if request.method == 'POST':
+       form = CategoriaForm(request.POST) # instancia o modelo com os dados do form
+       if form.is_valid():# faz a validação do formulário
+            form.save() # salva a instancia do modelo no banco de dados
+            return redirect('categoria') # redireciona para a listagem
+    else:# método é get, novo registro
+        form = CategoriaForm() # formulário vazio
     contexto = {
         'form':form,
     }
-    return render(resquest, 'categoria/formulario.html', contexto)
+    return render(request, 'categoria/formulario.html', contexto)
+
         
